97554b87f102006232a64597a85d20608b280669
Cole Martin <coolmn2016@gmail.com>
2021-06-27 17:15:30 -0500

    Movie Entry and learned M V C components

---
+++ ASPTutorial/Data/ASPTutorialContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace ASPTutorial.Data
+{
+    public class ASPTutorialContext : DbContext
+    {
+        public ASPTutorialContext (DbContextOptions<ASPTutorialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
